e9b91c8c84c14ed5c75a09cd93181c9aa7739b9b
venkatesh-buddy <venkatesh88a@gmail.com>
2020-11-01 20:51:41 +0530

    Spring Boot Oauth Google

---
+++ SpringBootOauthEg1/src/main/java/com/oauth/example/SpringBootOauthEg1Application.java
@@ -0,0 +1,13 @@
+package com.oauth.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthEg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauthEg1Application.class, args);
+	}
+
+}
---
+++ SpringBootOauthEg1/src/test/java/com/oauth/example/SpringBootOauthEg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauthEg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
